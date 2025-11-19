a918f4b2b308eed937ede7aff30bce01164dbb8d
KJFromMicromonic <prajwal@micromonic.com>
2025-11-19 15:23:55 +0100

    Fixed borken swc file

--- vitest.config.ts
+++ vitest.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     globals: true,
     include: ['services/__tests__/**/*.test.ts'],
     coverage: {
+      provider: 'v8',
       reporter: ['text', 'lcov'],
     },
   },
